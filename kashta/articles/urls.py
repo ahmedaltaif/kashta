14e735bf5f2dfd00081c25ed2f71895c16c3cd57
ahmedaltaif <118913943+ahmedaltaif@users.noreply.github.com>
2024-07-05 11:13:57 +0300

    Delete kashta directory

--- kashta/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),
-    path('topic/<int:pk>/', views.article_list, name='article_list'),
-    path('search/', views.search, name='search'),
-]
--- kashta/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('search/', views.search, name='search'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),
-]
