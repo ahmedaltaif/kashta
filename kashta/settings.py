19462ea05dfc6d1d66ab45d9162b8184a46227d7
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-07-05 17:57:42 +0300

    commit

--- kashta/settings.py
+++ kashta/settings.py
@@ -130,8 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 
 # IN PRODUCTION
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/") 
 
 
 # Default primary key field type
